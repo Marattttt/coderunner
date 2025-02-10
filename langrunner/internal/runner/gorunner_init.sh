a24693cf54d4f79dc61bdeff0c184e15d0c063db
Marat Bakasov <bakasovmarat04@gmail.com>
2025-02-10 20:36:26 +0600

    (chore): remove unneccesary code from gorunner init script

--- langrunner/internal/runner/gorunner_init.sh
+++ langrunner/internal/runner/gorunner_init.sh
@@ -1,14 +1,5 @@
 set -oue
 
-echo '------------------------'
-echo $USER
-echo '------------------------'
-
 go mod init tempfile
-
-# Add go symlink to curent dir
-ln -s $(which go) go
-chmod a+rx go
-chmod a+rx $(which go)
 
 exit 
