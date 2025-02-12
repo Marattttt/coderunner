37c992e77f27a7e72d3dbaeb38b359d06f7c23c2
Marat Bakasov <bakasovmarat04@gmail.com>
2025-02-12 20:33:52 +0600

    chore(front): adjust action button font size and make bold

--- frontend/src/components/common/ActionButton.tsx
+++ frontend/src/components/common/ActionButton.tsx
@@ -7,7 +7,14 @@ type ButtonProps = {
 
 const ActionButton: React.FC<ButtonProps> = ({ onClick, children }) => {
 	return (
-		<button onClick={() => onClick()} className="px-6 py-2 bg-bg-accent text-text-primary rounded-md hover:border-text-primary-2">
+		<button onClick={() => onClick()} 
+			className="px-6 py-2 bg-bg-accent 
+			font-bold text-xl text-text-primary rounded-xl 
+			transition duration-200
+			border-transparent
+			border-1
+			hover:border-text-primary"
+		>
 			{children}
 		</button>
 	);
