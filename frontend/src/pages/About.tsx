dcf4578daa1fb4694328cefa3b9268c45e56f2a6
Marat Bakasov <bakasovmarat04@gmail.com>
2025-02-16 17:52:13 +0600

    chore(front): minor padding corrections

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -14,8 +14,8 @@ const StackCard: React.FC<StackCardProps> = ({title,  items, className}) => {
 		<h3 className={'w-full text-center font-medium font-heading text-xl border-b-1 border-text-primary'}>
 			{title}
 		</h3>
-		<div className="flex flex-row justify-between g-6 w-full ">
-			{items.map((c) => (<div className={`flex flex-col w-full `} key={c.name}>
+		<div className="flex flex-row flex-wrap sm:flex-nowrap justify-between g-6 w-full ">
+			{items.map((c) => (<div className={`flex flex-col w-full p-2`} key={c.name}>
 				<h4 className="font-lg font-medium text-xl pl-4">
 					{c.name}
 				</h4>
