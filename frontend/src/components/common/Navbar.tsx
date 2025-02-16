f0541ad0e788c65a97273a769e6f9bbe3b643857
Marat Bakasov <bakasovmarat04@gmail.com>
2025-02-16 18:22:50 +0600

    feat(front): move navbar text to english locale json file

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -1,23 +1,26 @@
+import { useTranslation } from 'react-i18next'
 import EllipseSvg from '../../assets/Ellipse.svg?react'
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+	const {t} = useTranslation()
+
 	return (
 		<nav className="flex justify-between w-full px-4 sm:px-10 h-[64px] top-0 left-0 font-heading text-text-primary bg-bg-dark">
 			<Link
 				to="/"
 				className="bg-none my-auto border-none font-bold text-2xl"
 			>
-				Coderunner
+				{t("nav.title")}
 			</Link>
 
 			<div className="flex gap-8 align-middle justify-center bg-none border-none font-bold text-lg" >
 				<Link to="/editor" className="m-auto">
-					Editor
+					{t("nav.editor")}
 				</Link>
 				<EllipseSvg className="m-auto" />
 				<Link to="/about" className="m-auto">
-					About
+					{t("nav.about")}
 				</Link>
 			</div>
 		</nav>
