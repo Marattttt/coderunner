48a1e45e7f0ca83b85b3fb7fbd1feeb8efda587a
Marat Bakasov <bakasovmarat04@gmail.com>
2025-02-21 12:36:45 +0600

    refactor(langrunner): move routes into a different file

---
+++ langrunner/cmd/rest/routes.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/Marattttt/coderunner/langrunner/internal/config"
+	"github.com/Marattttt/coderunner/langrunner/internal/models"
+	"github.com/Marattttt/coderunner/langrunner/internal/runner"
+	"github.com/labstack/echo/v4"
+)
+
+func applyRoutes(e *echo.Echo, conf *config.AppConfig, logger *slog.Logger, runtimeManager *runner.RuntimeManager) {
+	e.POST("/api/run/go", runGoHandler(conf, logger, runtimeManager))
+	e.POST("/api/run/py", runPyHandler(conf, logger, runtimeManager))
+}
+
+func runPyHandler(conf *config.AppConfig, logger *slog.Logger, man *runner.RuntimeManager) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+
+		pyrunner := runner.NewPyRunner(logger, *conf.Python, man)
+
+		var req models.CodeReq
+
+		if err := json.NewDecoder(c.Request().Body).Decode(&req); err != nil {
+			return echo.NewHTTPError(
+				http.StatusBadRequest,
+				map[string]string{"error": "bad request"},
+			)
+		}
+
+		res, err := pyrunner.RunCode(ctx, []byte(req.Code))
+		if err != nil {
+			logger.Error("Failed to run code", slog.String("code", req.Code), slog.String("err", err.Error()))
+			return c.JSON(
+				http.StatusInternalServerError,
+				map[string]string{"error": "internal"},
+			)
+		}
+
+		return c.JSON(
+			http.StatusOK,
+			res,
+		)
+	}
+}
+
+func runGoHandler(conf *config.AppConfig, logger *slog.Logger, man *runner.RuntimeManager) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		ctx := c.Request().Context()
+
+		gorunner := runner.NewGoRunner(logger, *conf.Go, man)
+
+		var req models.CodeReq
+
+		if err := json.NewDecoder(c.Request().Body).Decode(&req); err != nil {
+			return echo.NewHTTPError(
+				http.StatusBadRequest,
+				map[string]string{"error": "bad request"},
+			)
+		}
+
+		res, err := gorunner.RunCode(ctx, []byte(req.Code))
+		if err != nil {
+			logger.Error("Failed to run code", slog.String("code", req.Code), slog.String("err", err.Error()))
+			return c.JSON(
+				http.StatusInternalServerError,
+				map[string]string{"error": "internal"},
+			)
+		}
+
+		return c.JSON(
+			http.StatusOK,
+			res,
+		)
+	}
+}
+
+func handlerParentContextMiddeware(ctx context.Context) func(echo.HandlerFunc) echo.HandlerFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.SetRequest(c.Request().WithContext(ctx))
+			return next(c)
+		}
+	}
+}
+
+func requestLogMiddleware(logger *slog.Logger) func(echo.HandlerFunc) echo.HandlerFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			req := c.Request()
+			res := c.Response()
+
+			reqId := c.Request().Header.Get(echo.HeaderXRequestID)
+
+			// Log request details
+			logger.Info("request",
+				slog.String("id", reqId),
+				slog.String("method", req.Method),
+				slog.String("path", req.URL.Path),
+				slog.String("remote_ip", c.RealIP()),
+			)
+
+			start := time.Now()
+
+			err := next(c)
+
+			end := time.Now()
+
+			// Log response details
+			if err != nil {
+				logger.Error("request error",
+					slog.String("id", reqId),
+					slog.String("error", err.Error()),
+					slog.Int64("timeTookMs", end.Sub(start).Milliseconds()),
+					slog.Int("status", res.Status),
+				)
+			} else {
+				logger.Info("response",
+					slog.String("id", reqId),
+					slog.Int("status", res.Status),
+					slog.Int64("timeTookMs", end.Sub(start).Milliseconds()),
+				)
+			}
+
+			return err
+		}
+	}
+}
